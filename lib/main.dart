20926f1fc48a56dfb9bc990382391b6c09394534
lilatee <marcin.hradowicz@gmail.com>
2024-12-05 15:38:18 +0100

    version 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 /// Entrypoint of the application.
@@ -22,20 +24,40 @@ class MyApp extends StatelessWidget {
               Icons.camera,
               Icons.photo,
             ],
-            builder: (e) {
-              return Container(
-                constraints: const BoxConstraints(minWidth: 48),
-                height: 48,
-                margin: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Colors.primaries[e.hashCode % Colors.primaries.length],
-                ),
-                child: Center(child: Icon(e, color: Colors.white)),
-              );
-            },
+            builder: (icon) => DockItem(
+              key: ValueKey(icon),
+              icon: icon,
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class DockItem extends StatelessWidget {
+  const DockItem({
+    super.key,
+    required this.icon,
+  });
+
+  static const double iconSize = 48;
+  static const double margin = 8;
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        height: iconSize,
+        width: iconSize,
+        margin: const EdgeInsets.all(margin),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.primaries[icon.hashCode % Colors.primaries.length],
+        ),
+        child: Center(child: Icon(icon, color: Colors.white)),
       ),
     );
   }
@@ -61,8 +83,34 @@ class Dock<T> extends StatefulWidget {
 
 /// State of the [Dock] used to manipulate the [_items].
 class _DockState<T> extends State<Dock<T>> {
+  static const double _padding = 4;
+
   /// [T] items being manipulated.
-  late final List<T> _items = widget.items.toList();
+  List<T> _items = [];
+
+  @override
+  void initState() {
+    _items = List.from(widget.items);
+
+    super.initState();
+  }
+
+  Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget? child) {
+        final double animValue = Curves.easeInOut.transform(animation.value);
+        final double scale = lerpDouble(1, 1.15, animValue)!;
+        return Transform.scale(
+          scale: scale,
+          child: DockItem(
+            icon: _items[index] as IconData,
+          ),
+        );
+      },
+      child: child,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,9 +119,25 @@ class _DockState<T> extends State<Dock<T>> {
         borderRadius: BorderRadius.circular(8),
         color: Colors.black12,
       ),
-      padding: const EdgeInsets.all(4),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
+      height: DockItem.iconSize + DockItem.margin * 2 + _padding * 2,
+      padding: const EdgeInsets.all(_padding),
+      child: ReorderableListView(
+        shrinkWrap: true,
+        proxyDecorator: proxyDecorator,
+        scrollDirection: Axis.horizontal,
+        onReorder: (oldIndex, newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+        prototypeItem: const SizedBox(
+          width: DockItem.iconSize + 2 * DockItem.margin,
+          height: DockItem.iconSize + 2 * DockItem.margin,
+        ),
         children: _items.map(widget.builder).toList(),
       ),
     );
